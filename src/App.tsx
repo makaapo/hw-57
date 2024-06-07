da06395c8cc41a70428033a1f108d20fa3269824
Karimov Abdumalik <abdumal1k.ur@gmail.com>
2024-06-07 07:05:39 +0600

    Created the User component and finished the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-const App = () => {
+import React, {useState} from 'react';
+import UserForm from './components/UserForm/UserForm';
+import Users from './components/Users/Users';
+import {User} from './types';
+
+const App: React.FC = () => {
+    const [users, setUsers] = useState<User[]>([]);
+
+    const AddUser = (user: User) => {
+        setUsers((prev) => [...prev, user]);
+    };
 
     return (
-        <div>
-           Hello
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-md-6">
+                    <UserForm onAddUser={AddUser}/>
+                </div>
+                <div className="col-md-6">
+                    <Users users={users}/>
+                </div>
+            </div>
         </div>
     );
 };
